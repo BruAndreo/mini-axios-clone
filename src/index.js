54250246b55a7e0a9e06aa7096aa86892c5f9ee3
Bruno Andreo <brunolino2026@gmail.com>
2021-02-20 11:01:29 -0300

    finish tests and incluind generators and interators

--- src/index.js
+++ src/index.js
@@ -1,37 +1,16 @@
-const Request = require("./request");
+const Pagination = require("./pagination");
 
-const request  = new Request();
-
-async function scheduler() {
-    console.log('Starting in... ', new Date().toISOString());
+;(async () => {
+    const pagination = new Pagination();
     
-    const requests = [
-        { url: 'https://www.mercadobitcoin.net/api/BTC/ticker/' },
-        { url: 'https://www.nao_existe.net' },
-        { url: 'https://www.mercadobitcoin.net/api/BTC/orderbook/' }
-    ].map(data => ({
-        ...data,
-        timeout: 2000,
-        method: 'get'
-    }))
-    .map(params => request.makeRequest(params));
+    const firstPage = 770e3; //77000
 
-    const result = await Promise.allSettled(requests);
+    const req = pagination.getPaginated({
+        url: 'https://www.mercadobitcoin.net/api/BTC/trades/',
+        page: firstPage
+    });
 
-    const allSucceded = [];
-    const allFailed = [];
-
-    for(const { status, value, reason } of result) {
-        if (status === 'rejected') {
-            allFailed.push(reason);
-            continue;
-        }
-
-        allSucceded.push(value);
+    for await (const items of req) {
+        console.table(items)
     }
-
-    console.log({ allFailed, allSucceded });
-}
-
-const PERIOD = 2000;
-setInterval(scheduler, PERIOD);
+})()
